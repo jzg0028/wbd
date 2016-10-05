97411106b7b75a325806dc1d83845be1a5a6ac62
Jesse <jzg0028@auburn.edu>
2016-10-05 12:59:06 -0500

    skeleton code for Fix and FixTest

---
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -0,0 +1,16 @@
+"""
+    October 5th 2016
+
+    author: Jesse Gamez
+"""
+
+class Fix():
+
+    def __init__(self, name):
+        pass
+
+    def setSightingFile(self, name):
+        pass
+
+    def getSightings():
+        pass
---
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -0,0 +1,13 @@
+import unittest
+from Navigation.prod.Fix import Fix
+
+class FixTest(unittest.TestCase):
+
+    def constructorTest(self):
+        pass
+
+    def setSightingFileTest(self):
+        pass
+
+    def getSightingsTest(self):
+        pass
