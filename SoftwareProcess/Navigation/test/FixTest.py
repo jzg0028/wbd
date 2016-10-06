9d847dc80444c3e56384c2ff97ba8bc383265e17
Jesse <jzg0028@auburn.edu>
2016-10-06 17:47:15 -0500

    setUp and tearDown for deleting and closing files

--- SoftwareProcess/Navigation/test/FixTest.py
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -1,10 +1,19 @@
 import unittest
+import os
 from Navigation.prod.Fix import Fix
 
 # Note: ISO 8601 UTC date/time
 class FixTest(unittest.TestCase):
 
-    def constructorTest(self):
+    def setUp(self):
+        self.files = []
+
+    def tearDown(self):
+        for f in self.files:
+            f.close()
+            os.remove(f.name)
+
+    def testConstructor(self):
     # these are invalid names
         with self.assertRaises(ValueError):
             Fix("file.not-a-txt")
@@ -13,27 +22,27 @@ class FixTest(unittest.TestCase):
 
     # these files should not have been created
         with self.assertRaises(FileNotFoundError):
-            open("file.not-a-txt", "r")
+            self.files.append(open("file.not-a-txt", "r"))
         with self.assertRaises(FileNotFoundError):
-            open(".txt", "r")
+            self.files.append(open(".txt", "r"))
 
     # default file name
         try:
             Fix()
-            open("log.txt", "r")
+            self.files.append(open("log.txt", "r"))
         except (FileNotFoundError, ValueError) as e:
             self.fail(e)
 
     # valid file name
         try:
             Fix("foo.txt")
-            open("foo.txt", "r")
+            self.files.append(open("foo.txt", "r"))
         except (FileNotFoundError, ValueError) as e:
             self.fail(e)
 
 # instructions are unclear
 # append "Start of sighting file: f.xml" to log
-    def setSightingFileTest(self):
+    def testSetSightingFile(self):
     # these are invalid names
         with self.assertRaises(ValueError):
             Fix("file.not-an-xml")
@@ -42,11 +51,9 @@ class FixTest(unittest.TestCase):
 
     # these files should not have been created
         with self.assertRaises(FileNotFoundError):
-            open("file.not-an-xml", "r")
+            self.files.append(open("file.not-an-xml", "r"))
         with self.assertRaises(FileNotFoundError):
-            open(".xml", "r")
+            self.files.append(open(".xml", "r"))
 
-# append sightings from XML file to log
-# apped "End of sighting file: f.xml" to log
-    def getSightingsTest(self):
+    def testGetSightings(self):
         pass
