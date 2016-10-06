7fc46997398269eb7479d41b60ba85ebc4779e01
Jesse <jzg0028@auburn.edu>
2016-10-06 17:53:32 -0500

    filename validation

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -1,16 +1,29 @@
 """
     October 5th 2016
+    October 6th 2016
 
     author: Jesse Gamez
 """
 
 class Fix(object):
 
-    def __init__(self, name):
-        pass
+    def __init__(self, name = "log.txt"):
+        if name.count(".txt") == 0 or name[-5:] == ".txt":
+            raise ValueError
+
+    # must store name instead of FD to avoid zombie FD
+        self.log = name
+        f = open(name, "a")
+        f.close()
 
     def setSightingFile(self, name):
-        pass
+        if name.count(".xml") == 0 or name[-5:] == ".xml":
+            raise ValueError
+
+    # must store name instead of FD to avoid zombie FD
+        self.sightings = name
+        f = open(name, "a")
+        f.close()
 
     def getSightings():
         pass
