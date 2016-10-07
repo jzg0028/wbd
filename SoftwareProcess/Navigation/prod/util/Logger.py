8a6cf032465caac31231c8332df2813953dac3cc
Jesse <jzg0028@auburn.edu>
2016-10-07 12:20:08 -0500

    added UTC offset to timestamp; removed microseconds

--- SoftwareProcess/Navigation/prod/util/Logger.py
+++ SoftwareProcess/Navigation/prod/util/Logger.py
@@ -4,11 +4,18 @@
     author: Jesse Gamez
 """
 
-from datetime import datetime
+from datetime import datetime, tzinfo, timedelta
+import time
+
+class TimeZone(tzinfo):
+    def utcoffset(self, dt):
+        return -timedelta(seconds = time.timezone)
 
 def logify(string):
-    return ("LOG: " +
-        datetime.utcnow().isoformat(' ') +
-        ": " +
+    return ("LOG:\t" +
+        datetime.utcnow()
+            .replace(tzinfo = TimeZone(), microsecond = 0)
+            .isoformat(' ') +
+        ":\t" +
         string +
         "\n")
