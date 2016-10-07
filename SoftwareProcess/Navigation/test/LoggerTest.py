4df865638d85fa2dbe67b83785948828158f651a
Jesse <jzg0028@auburn.edu>
2016-10-06 22:44:23 -0500

    wrote test for log entry formatter

---
+++ SoftwareProcess/Navigation/test/LoggerTest.py
@@ -0,0 +1,18 @@
+import unittest
+import re
+
+import Navigation.prod.util.Logger as Logger
+
+class LoggerTest(unittest.TestCase):
+
+    def testLogifyTimestamp(self):
+        regex = re.compile("LOG: \d{4}(-\d{2}){2} (\d{2}:?){3}\.\d{6}:")
+        actual = Logger.logify("foobar")
+        self.assertTrue(regex.match(actual), "bad timestamp: " + actual)
+
+    def testLogifyNewline(self):
+        self.assertEqual("\n", Logger.logify("foobar")[-1])
+
+    def testLogifyMessage(self):
+        foo = "foobar"
+        self.assertEqual(foo, Logger.logify(foo)[-len(foo) - 1 : -1])
