907b2013317bd773572956851bbc758e35d57b4a
Jesse <jzg0028@auburn.edu>
2016-10-29 18:45:03 -0500

    Green Light |

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -14,7 +14,11 @@ class Fix(object):
 
     def __init__(self, name = "log.txt"):
         if name.count(".txt") == 0 or name[-5:] == ".txt":
-            raise ValueError
+            raise ValueError (
+                self.__class__.__name__ + "."
+                + self.__init__.__name__
+                + ": invalid filename"
+            )
 
     # must store name instead of FD to avoid zombie FD
         self.log = name
@@ -23,11 +27,19 @@ class Fix(object):
             with open(name, "a") as log:
                 log.write(Logger.logify("Start of log"))
         except(IOError):
-            raise ValueError
+            raise ValueError (
+                self.__class__.__name__ + "."
+                + self.__init__.__name__
+                + ": invalid filename"
+            )
 
     def setSightingFile(self, name):
         if name.count(".xml") == 0 or name[-5:] == ".xml":
-            raise ValueError
+            raise ValueError (
+                self.__class__.__name__ + "."
+                + self.setSightingFile.__name__
+                + ": invalid filename"
+            )
 
     # store name instead of FD to avoid zombie FD
         self.sightings = name
@@ -36,9 +48,20 @@ class Fix(object):
             with open(name, "r") as sightings, open(self.log, "a") as log:
                 log.write(Logger.logify("Start of sighting file: " + name))
         except(IOError):
-            raise ValueError
+            raise ValueError (
+                self.__class__.__name__ + "."
+                + self.setSightingFile.__name__
+                + ": invalid filename"
+            )
 
     def getSightings(self):
+        if not hasattr(self, 'sightings'):
+            raise ValueError (
+                self.__class__.__name__ + "."
+                + self.getSightings.__name__
+                + ": sighting file not set"
+            )
+
     # any errors at this point are undefined
         with open(self.log, "a") as log:
             for sighting in Sighting.parse(self.sightings):
