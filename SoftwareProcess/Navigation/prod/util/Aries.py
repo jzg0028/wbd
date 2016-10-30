cec71b6e73c6bfb3ff65de2128d0b266b574c12c
Jesse <jzg0028@auburn.edu>
2016-10-30 12:37:16 -0500

    Green Test |

---
+++ SoftwareProcess/Navigation/prod/util/Aries.py
@@ -0,0 +1,20 @@
+class Aries(object):
+
+    def __init__(self, fname, date, hour):
+        line = self.search(fname, date, hour)
+
+        if line == None:
+            raise ValueError("observed date too late or early")
+
+        self.gha = line[2][0:-1]
+
+    def search(self, fname, date, hour):
+        with open(fname, 'r') as ariesFile:
+
+            for line in ariesFile:
+                line = line.split('\t')
+
+                if line[0] == date and line[1] == hour:
+                    return line
+
+            return None
