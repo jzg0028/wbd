da92393b0788280e874ae5804e8e359aed95f31f
Jesse <jzg0028@auburn.edu>
2016-10-30 13:55:15 -0500

    Red Light |

--- SoftwareProcess/Navigation/test/SightingTest.py
+++ SoftwareProcess/Navigation/test/SightingTest.py
@@ -5,8 +5,17 @@ import Navigation.prod.util.Sighting as Sighting
 class SightingTest(unittest.TestCase):
 
     def testToString(self):
-        fname = "SoftwareProcess/Navigation/resources/sightings.xml"
-        arr = Sighting.parse(fname)
+        root = "SoftwareProcess/Navigation/resources/"
+        sightingFile = root + "sightings.xml"
+        ariesFile = root + "aries.txt"
+        starFile = root + "stars.txt"
+        arr = Sighting.parse(sightingFile, starFile, ariesFile)
+
+        self.assertEqual("23", arr[0].hour())
+        self.assertEqual("9", arr[1].hour())
+
+        self.assertEqual("04/14/17", arr[0].date())
+        self.assertEqual("04/14/17", arr[1].date())
 
         self.assertEqual (
             "Pollux\t2017-04-14\t23:50:14\t15d01.5\t27d59.1\t84d33.4",
