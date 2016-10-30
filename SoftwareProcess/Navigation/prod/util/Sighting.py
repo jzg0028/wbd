3c054f7e8ef2c73f706c73451c137c1eb95599fd
Jesse <jzg0028@auburn.edu>
2016-10-30 13:55:27 -0500

    Green Light |

--- SoftwareProcess/Navigation/prod/util/Sighting.py
+++ SoftwareProcess/Navigation/prod/util/Sighting.py
@@ -2,14 +2,22 @@ import xml.etree.ElementTree as ET
 import math
 
 from Navigation.prod.Angle import Angle
+from Navigation.prod.util.Star import Star
+from Navigation.prod.util.Aries import Aries
+import re
 
-def parse(fname):
-    return tuple((Sighting(sighting))
-        for sighting in ET.parse(fname).getroot())
+def parse(sightingFile, starFile, ariesFile):
+    return tuple((Sighting(sighting, starFile, ariesFile))
+        for sighting in ET.parse(sightingFile).getroot())
 
 class Sighting(object):
-    def __init__(self, node):
+    def __init__(self, node, starFile, ariesFile):
         self.arr = dict((child.tag, child.text) for child in node)
+
+        date = self.date()
+        hour = self.hour()
+        self.star = Star(starFile, self.arr["body"], date)
+        self.aries = Aries(ariesFile, date, hour)
 
     def adjustedAltitude(self):
         altitude = Angle().setDegreesAndMinutes(self.arr["observation"])
@@ -22,8 +30,31 @@ class Sighting(object):
         else (-0.97 * math.sqrt(float(self.arr["height"]))) / 60)
 
         return altitude + dip + refraction
+
+    def geographicLatitude(self):
+        return self.star.declination
+
+    def date(self):
+        match = re.compile("^\d\d(\d\d)-(\d\d)-(\d\d)$").match(self.arr["date"])
+
+        return match.group(2) + "/" + match.group(3) + "/" + match.group(1)
+
+    def hour(self):
+        match = re.compile("^0?([1-9]?\d):\d\d:\d\d$").match(self.arr["time"])
+
+        return match.group(1)
+
+    def geographicLongitude(self):
+        sha, gha = (Angle(),)*2
+        sha.setDegreesAndMinutes(self.star.sha)
+        gha.setDegreesAndMinutes(self.aries.gha)
+
+        return sha.add(gha)
+
     def __str__(self):
         return (self.arr["body"]
             + "\t" + self.arr["date"]
             + "\t" + self.arr["time"]
-            + "\t" + Angle(self.adjustedAltitude()).getString())
+            + "\t" + Angle(self.adjustedAltitude()).getString()
+            + "\t" + Angle(self.geographicLatitude()).getString()
+            + "\t" + Angle(self.geographicLongitude()).getString())
