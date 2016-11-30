862a3fbbf9afc32618d93bc64661a8c504a5aaf7
Jesse <jzg0028@auburn.edu>
2016-11-29 20:53:12 -0600

    made Sighting more testable

--- SoftwareProcess/Navigation/prod/util/Sighting.py
+++ SoftwareProcess/Navigation/prod/util/Sighting.py
@@ -6,110 +6,67 @@ from Navigation.prod.util.Star import Star
 from Navigation.prod.util.Aries import Aries
 import re
 
-class Sighting(object):
-    def __init__(self, node, starFile,
-        ariesFile, assumedLatitude, assumedLongitude):
+class GeographicPosition(object):
+    def __init__(self, star, aries1, aries2, seconds):
+        self.dLong = (Angle().setDegreesAndMinutes(star.hda)
+        + (Angle().setDegreesAndMinutes(aries1.gha)
+        + (Angle().setDegreesAndMinutes(aries2.gha)
+        - Angle().setDegreesAndMinutes(aries1.gha))
+        * seconds / 3600))
 
-        self.arr = dict((child.tag, child.text) for child in node)
+        self.dLat = (Angle().setDegreesAndMinutes(star.declination))
 
-        self.assLat, self.assLon = assumedLatitude, assumedLongitude
+    def latitude(self):
+        return Angle(self.dLat)
 
-        date = self.date()
-        hour = self.hour()
-        self.star = Star(starFile, self.arr["body"], date)
-        self.aries1 = Aries(ariesFile, date, str(hour))
-        self.aries2 = Aries(ariesFile, date, str((hour + 1) % 24))
+    def longitude(self):
+        return Angle(self.dLong)
 
-    def adjustedAltitude(self):
-        altitude = Angle().setDegreesAndMinutes(self.arr["observation"])
+class Adjustment(object):
+# CONVERT PRESSURE, TEMPERATURE TO INT
+# HEIGHT TO FLOAT
+    def __init__(self, observation, pressure,
+        temperature, horizon, height, geographicPosition,
+        assumedLatitude, assumedLongitude):
 
-        refraction = ((-0.00452 * int(self.arr["pressure"]))
-            / (273 + ((int(self.arr["temperature"]) - 32) * 5 / 9))
-            / math.tan(math.radians(altitude)))
+        (self.observation, self.pressure,
+        self.temperature, self.horizon,
+        self.height, self.geographicPosition,
+        self.assumedLatitude,
+        self.assumedLongitude) = (Angle().setDegreesAndMinutes(observation),
+        pressure, temperature, horizon == 'Artificial',
+        height, geographicPosition,
+        Angle(Angle().setDegreesAndMinutes(assumedLatitude)),
+        Angle(Angle().setDegreesAndMinutes(assumedLongitude)))
 
-        dip = (0 if self.arr["horizon"] == "Artificial"
-        else (-0.97 * math.sqrt(float(self.arr["height"]))) / 60)
-
-        return Angle(altitude + dip + refraction)
-
-    def geographicLatitude(self):
-        a = Angle()
-        a.setDegreesAndMinutes(self.star.declination)
-        return a
-
-    def date(self):
-        match = re.compile("^\d\d(\d\d)-(\d\d)-(\d\d)$").match(self.arr["date"])
-
-        return match.group(2) + "/" + match.group(3) + "/" + match.group(1)
-
-    def hour(self):
-        match = re.compile("^(\d\d):\d\d:\d\d$").match(self.arr["time"])
-
-        return int(match.group(1))
-
-    def seconds(self):
-        match = re.compile("^\d\d:(\d\d):(\d\d)").match(self.arr["time"])
-        
-        return (int(match.group(1)) * 60) + int(match.group(2))
-
-    def geographicLongitude(self):
-        hda = Angle()
-        hda.setDegreesAndMinutes(self.star.hda)
-        hda.add(self.ghaAries())
-        return hda
-
-    def ghaAries(self):
-        gha1 = Angle()
-        gha1.setDegreesAndMinutes(self.aries1.gha)
-
-        gha1.add(Angle(self.subAries().getDegrees() * self.seconds() / 3600))
-
-        return gha1
-
-    def subAries(self):
-        gha1 = Angle()
-        gha2 = Angle()
-        gha1.setDegreesAndMinutes(self.aries1.gha)
-        gha2.setDegreesAndMinutes(self.aries2.gha)
-
-        gha2.subtract(gha1)
-
-        return gha2
-
-    def localHourAngle(self):
+    def altitude(self):
         return Angle (
-            self.geographicLongitude().getDegrees()
-            - Angle().setDegreesAndMinutes(self.assLon)
+            self.observation
+            + (0 if self.horizon else (-0.97 * math.sqrt(self.height)) / 60)
+            + ((-0.00452 * self.pressure)
+            / (273 + ((self.temperature - 32) * 5 / 9))
+            / math.tan(math.radians(self.observation)))
         )
 
-    def correctedAltitude(self):
-        rGeoLat = math.radians(self.geographicLatitude().getDegrees())
-        rAssLat = math.radians(Angle().setDegreesAndMinutes(self.assLat[1:]))
-        rLHA = math.radians(self.localHourAngle().getDegrees())
+    def distance(self):
+        rGeoLat = math.radians(self.geographicPosition.dLat)
+        rAssLat = math.radians(self.assumedLatitude.getDegrees())
+        rLHA = math.radians (
+            self.geographicPosition.dLong - self.assumedLatitude.getDegrees()
+        )
 
-        return Angle (
-            math.asin (
-                (math.sin(rGeoLat)
+        return int(round((self.altitude().getDegrees()
+                - math.asin((math.sin(rGeoLat)
                 * math.sin(rAssLat))
                 + (math.cos(rGeoLat)
                 * math.cos(rAssLat)
-                * math.cos(rLHA))
-            )
-        )
+                * math.cos(rLHA))))
+                * 60))
 
-    def distanceAdjustment(self):
-        return int (
-            round (
-                (self.adjustedAltitude().getDegrees()
-                - self.correctedAltitude().getDegrees())
-                * 60
-            )
-        )
-
-    def azimuthAdjustment(self):
-        rGeoLat = math.radians(self.geographicLatitude().getDegrees())
-        rAssLat = math.radians(Angle().setDegreesAndMinutes(self.assLat[1:]))
-        rDisAdj = math.radians (self.distanceAdjustment() / 60.0)
+    def azimuth(self):
+        rGeoLat = math.radians(self.geographicPosition.dLat)
+        rAssLat = math.radians(self.assumedLatitude.getDegrees())
+        rDisAdj = math.radians(self.distance() / 60.0)
 
         return Angle (
             math.acos (
@@ -121,13 +78,51 @@ class Sighting(object):
             )
         )
 
+class Sighting(object):
+    def __init__(self, node, starFile,
+        ariesFile, assumedLatitude, assumedLongitude):
+
+        arr = dict((child.tag, child.text) for child in node)
+
+        (self.assLat, self.assLon, self.date,
+        self.time, self.body) = (assumedLatitude,
+        assumedLongitude, arr['date'],
+        arr['time'], arr['body'])
+
+        match = re.compile('^\d\d(\d\d)-(\d\d)-(\d\d)$') \
+            .match(arr['date'])
+        date = match.group(2) + '/' + match.group(3) + '/' + match.group(1)
+        
+        match = re.compile('^(\d\d):(\d\d):(\d\d)$').match(arr['time'])
+        hour = int(match.group(1))
+        seconds = (int(match.group(2)) * 60) + int(match.group(3))
+
+        self.geographicPosition = GeographicPosition (
+            Star(starFile, arr['body'], date),
+            Aries(ariesFile, date, str(hour)),
+            Aries(ariesFile, date, str((hour + 1) % 24)),
+            seconds
+        )
+
+        self.adjustment = Adjustment (
+            arr['observation'],
+            int(arr['pressure']),
+            int(arr['temperature']),
+            arr['horizon'],
+            float(arr['height']),
+            self.geographicPosition,
+        # negative angle is S, else positive
+            self.assLat.replace('S', '-').replace('N', ''),
+            self.assLon
+        )
+
     def __str__(self):
-        return (self.arr["body"]
-            + "\t" + self.arr["date"]
-            + "\t" + self.arr["time"]
-            + "\t" + self.adjustedAltitude().getString()
-            + "\t" + self.geographicLatitude().getString()
-            + "\t" + self.geographicLongitude().getString()
+        return (self.body
+            + "\t" + self.date
+            + "\t" + self.time
+            + "\t" + self.adjustment.altitude().getString()
+            + "\t" + self.geographicPosition.latitude().getString()
+            + "\t" + self.geographicPosition.longitude().getString()
             + "\t" + self.assLat + "\t" + self.assLon
-            + "\t" + self.azimuthAdjustment().getString()
-            + "\t" + str(self.distanceAdjustment()))
+            + "\t" + self.adjustment.azimuth().getString()
+            + "\t" + str(self.adjustment.distance()))
