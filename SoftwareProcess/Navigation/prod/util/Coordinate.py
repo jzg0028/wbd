83ca9a9439ac11bf1e4c314a98ee55bdafa9de4b
Jesse <jzg0028@auburn.edu>
2016-11-30 17:12:24 -0600

    Green Light |

--- SoftwareProcess/Navigation/prod/util/Coordinate.py
+++ SoftwareProcess/Navigation/prod/util/Coordinate.py
@@ -1,44 +1,34 @@
 from Navigation.prod.Angle import Angle
-import re
 
 class Coordinate(object):
-    def __init__(self, latitude = '0d0.0', longitude = '0d0.0'):
-        self.setDegreesAndMinutes(latitude, longitude)
+    def __init__(self, latitude, longitude):
+        if type(latitude) is Angle and type(longitude) is Angle:
+            self.setByAngle(latitude, longitude)
+        elif type(latitude) is str and type(longitude) is str:
+            self.setByString(latitude, longitude)
+        else:
+            self.setByDegree(latitude, longitude)
+    
+    def latStr(self):
+        return (('' if self.lat % 90 == 0 else
+        'N' if self.lat > 0 else 'S')
+        + ("%dd%.1f" % (int(abs(self.lat) % 90),
+        (self.lat % 1) * 60)))
 
-    def setDegreesAndMinutes(self, latitude, longitude):
-        if not re.compile (
-            '^(?:0?0?0d0?0.0|' \
-            + '[SN]0?(?:(?:(?:[1-8]\d|' \
-            + '0?[1-9])d[0-5]?\d\.\d)|' \
-            + '0?0d(?:[1-5]\d\.\d|' \
-            + '0?[1-9]\.\d|' \
-            + '0?0\.[1-9])))$'
-        ).match(latitude):
-            raise ValueError("Coordinate: bad latitude: " + latitude)
-        self.latitude = latitude
+    def lonStr(self):
+        return self.lon.getString()
 
-        if not re.compile('^(?:[0-2]?\d?\d|3[0-5]\d)d(?:[0-5]?\d\.\d)$') \
-            .match(longitude):
-            raise ValueError("Coordinate: bad longitude: " + longitude)
-        self.longitude = longitude
-
-    def setDegrees(self, latitude, longitude):
-        self.setLatitudeString(
-            ('S' if latitude < 0 else 'N' if latitude > 0 else '')
-            + Angle(latitude).getString().replace('-', ''))
-
-        self.setLongitudeString(Angle(longitude).getString())
-
-    def latitudeAngle(self):
-        return Angle (
-            Angle().setDegreesAndMinutes (
-                self.latitude.replace('S', '-').replace('N', '')
-            )
+    def setByString(self, latitude, longitude):
+        self.setByDegree (
+            Angle(latitude.replace('S', '-').replace('N', '')).angle,
+            Angle(longitude).angle
         )
 
-    def longitudeAngle(self):
-        return Angle (
-            Angle().setDegreesAndMinutes (
-                self.longitude
-            )
+    def setByAngle(self, latitude, longitude):
+        self.setByDegree (
+            latitude.angle,
+            longitude.angle
         )
+
+    def setByDegree(self, latitude, longitude):
+        self.lat, self.lon = latitude, longitude
