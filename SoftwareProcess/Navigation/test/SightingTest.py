67f80467d0652d03068e3ab8a155d34f2bbcc6f2
Jesse <jzg0028@auburn.edu>
2016-11-30 19:49:15 -0600

    Red Light |

--- SoftwareProcess/Navigation/test/SightingTest.py
+++ SoftwareProcess/Navigation/test/SightingTest.py
@@ -6,64 +6,64 @@ from Navigation.prod.util.Coordinate import Coordinate
 
 class SightingTest(unittest.TestCase):
 
-    def getSightings(self, root, sighting, star, aries):
+    def getSightings(self, root, sighting, star, aries, lat, lon):
         sighting = root + sighting
         star = root + star
         aries = root + aries
 
-        return [Sighting(node, star, aries, Coordinate('S1d0.0', '0d0.0'))
+        return [Sighting(node, star, aries, Coordinate(lat, lon))
             for node in ET.parse(sighting).getroot()]
     
     def testToString(self):
         arr = self.getSightings (
             'SoftwareProcess/Navigation/resources/',
-            'sightings.xml',
+            'pollux.xml',
             'stars.txt',
-            'aries.txt'
+            'aries.txt',
+            'S53d38.4',
+            '74d35.3'
         )
 
         self.assertEqual (
-            "Pollux\t2017-04-14\t23:50:14\t15d1.5\t27d59.1\t83d43.8",
+            "Pollux\t2017-04-14\t23:50:14\t15d1.5\t27d59.1\t" \
+            + "84d33.4\tN27d59.5\t85d33.4\t292d44.6\t174",
             str(arr[0])
-        )
-        self.assertEqual (
-            "Sirius\t2017-04-17\t09:30:30\t45d11.9\t343d15.5\t247d6.2",
-            str(arr[1])
         )
 
     def testGeographicLongitude(self):
         arr = self.getSightings (
             'SoftwareProcess/Navigation/resources/',
-            'sightings.xml',
+            'pollux.xml',
             'stars.txt',
             'aries.txt'
+            'S53d38.4',
+            '74d35.3'
         )
 
-        self.assertEqual('83d43.8', arr[0].geographicPosition
-            .longitude().getString())
-        self.assertEqual('247d6.2', arr[1].geographicPosition
+        self.assertEqual('84d33.4', arr[0].geographicPosition
             .longitude().getString())
 
     def testGeographicLatitude(self):
         arr = self.getSightings (
             'SoftwareProcess/Navigation/resources/',
-            'sightings.xml',
+            'pollux.xml',
             'stars.txt',
             'aries.txt'
+            'S53d38.4',
+            '74d35.3'
         )
 
         self.assertEqual('27d59.1', arr[0].geographicPosition
-            .latitude().getString())
-        self.assertEqual('343d15.5', arr[1].geographicPosition
             .latitude().getString())
 
     def testAdjustedAltitude(self):
         arr = self.getSightings (
             'SoftwareProcess/Navigation/resources/',
-            'sightings.xml',
+            'pollux.xml',
             'stars.txt',
             'aries.txt'
+            'S53d38.4',
+            '74d35.3'
         )
 
         self.assertEqual('15d1.5', arr[0].adjustment.altitude().getString())
-        self.assertEqual('45d11.9', arr[1].adjustment.altitude().getString())
