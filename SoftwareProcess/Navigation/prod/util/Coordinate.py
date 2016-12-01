77dac5b6c8eb83bd9c6da0a07e69a974c7bcf409
Jesse <jzg0028@auburn.edu>
2016-11-30 18:57:57 -0600

    added error checking to Coordinate

--- SoftwareProcess/Navigation/prod/util/Coordinate.py
+++ SoftwareProcess/Navigation/prod/util/Coordinate.py
@@ -1,4 +1,5 @@
 from Navigation.prod.Angle import Angle
+import re
 
 class Coordinate(object):
     def __init__(self, latitude, longitude):
@@ -19,6 +20,19 @@ class Coordinate(object):
         return Angle(self.lon).getString()
 
     def setByString(self, latitude, longitude):
+        if not re.compile (
+            '^(0?0?0d0?0\.0|' \
+            + '[NS]0?(([1-8]\d|' \
+            + '0?[1-9])d(\d?\d\.\d)|'
+            + '0?0?0d([1-5]\d\.\d|'
+            + '0?[1-9]\.\d|'
+            + '0?0\.[1-9])))$'
+        ).match(latitude):
+            raise ValueError('Coordinate: bad latitude: ' + latitude)
+        if not re.compile('^([0-2]?\d?\d|3[0-5]\d)d[0-5]?\d\.\d$') \
+            .match(longitude):
+            raise ValueError('Coordinate: bad longitude: ' + longitude)
+
         self.setByDegree (
             Angle(latitude.replace('S', '-').replace('N', '')).angle,
             Angle(longitude).angle
